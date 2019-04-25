0bb634647738d41761fb1d013cd804f36b8576bb
Vanessa Mastrosimone <vlmastrosimone@gmail.com>
2019-04-24 21:15:03 -0700

    added deleteUser and getAllUser functions with working SQL

---
+++ deleteUser/index.js
@@ -0,0 +1,31 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request
+var TYPES = require('tedious').TYPES;
+var config = require('../creds.js');
+
+var connection = new Connection(config);
+
+connection.on('connect', function(err){
+    if (err) {
+        console.log(err)
+    }
+    else {
+        deleteUser();
+    }
+});
+
+function deleteUser()
+{
+    console.log('Attempting to delete user...');
+
+    // Read all rows from table
+    var request = new Request(
+        "DELETE FROM Users WHERE uID = '5'",
+        function(err, rowCount, rows)
+        {
+            process.exit();
+        }
+    );
+
+    connection.execSql(request);
+}
---
+++ getAllUsers/index.js
@@ -0,0 +1,38 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request
+var TYPES = require('tedious').TYPES;
+var config = require('../creds.js');
+
+var connection = new Connection(config);
+
+connection.on('connect', function(err){
+    if (err) {
+        console.log(err)
+    }
+    else {
+        getAllUsers();
+    }
+});
+
+function getAllUsers()
+{
+    console.log('Reading rows from the Table...');
+
+    // Read all rows from table
+    var request = new Request(
+        "SELECT * FROM Users",
+        function(err, rowCount, rows)
+        {
+            console.log(rowCount + ' uers');
+            process.exit();
+        }
+    );
+
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+
+    connection.execSql(request);
+}
