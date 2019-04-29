b1a3e1c6e0ce62ecfbbdc7c51b9c0b51f3fde681
Alex Lopez <alexjlopez96@gmail.com>
2019-04-29 10:09:57 -0700

    close but no cigar -- AF links not returning data

--- Orchestrator/index.js
+++ Orchestrator/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
     var Connection = require('tedious').Connection;
     var Request = require('tedious').Request;
-    var config = require('../creds.js');
+    var config = require('./creds.js');
 
     var connection = new Connection(config);
 
@@ -12,38 +12,36 @@ module.exports = async function (context, req) {
             "getAllRobots":"SELECT * FROM Robots",
             "getAllTimeSlots": "SELECT * FROM TimeSlots", 
 
-            "addUser": "INSERT INTO Users (uID) VALUES (5)", 
-            "addTimeSlot": "INSERT INTO TimeSlots (TimeID) VALUES (5)",
-            "addRobot": "INSERT INTO Robot (RobotID) VALUES (5)",
+            "addUserOld": "INSERT INTO Users (uID) VALUES (5)",
+            "addUser": "INSERT INTO Users DEFAULT VALUES",
+            "addTimeSlotOld": "INSERT INTO TimeSlots (TimeID) VALUES (5)",
+            "addTimeSlot": "INSERT INTO TimeSlots(IsReserved, StartTime, EndTime) VALUES (0,07:30,10:30)",
+            "addRobotOld": "INSERT INTO Robots (RobotID) VALUES (5)", 
+            "addRobot": "INSERT INTO Robots(IsReserved) VALUES (0)",
 
             "deleteUser": "DELETE FROM Users WHERE uID = '25'",
             "deleteTimeSlot": "DELETE FROM TimeSlots WHERE TimeID = '5'",
-            "deleteRobot": "DELETE FROM Robot WHERE RobotID = '5'"
+            "deleteRobot": "DELETE FROM Robots WHERE RobotID = '5'"
         };
 
-        // execDbCommand(routes[req[]])
-
         if (err) {
-            console.log(err);
+            context.log(err);
         }
         else {
-            // execDbCommand("SELECT * FROM Users");
-            // execDbCommand(routes["getAllUsers"]);
-            if (req.query.func) {
-                var returnData = execDbCommand(routes[req.query.func])
-                context.res = {
-                    status: 200, 
-                    body: returnData
-                };
-            }
-            else { 
-                context.res = {
-                    status: 400,
-                    body: "Please pass a name on the query string or in the request body"
-                };
-            }
-            // execDbCommand(routes[req.query.func])
+            execDbCommand(routes[req.query.func])
         }
+
+        /*
+        if (req.query.name || (req.body && req.body.name)) {
+            execDbCommand(routes[req.query.func])
+        }
+        else {
+            context.res = {
+                status: 400,
+                body: "Please pass a name on the query string or in the request body"
+            };
+        }
+        */
     });
 
 
@@ -54,25 +52,36 @@ module.exports = async function (context, req) {
     {
         console.log('Reading rows from the Table...');
 
+        var retval = ' ';
+
         // Read all rows from table
         var request = new Request(
             sqlStatement,
             function(err, rowCount, rows)
             {
-                console.log(rowCount + ' rows returned');
-                // console.log(rows);
+                context.log(rowCount + ' rows returned');
+                // context.log(retval);
+            
+                context.res = {
+                    status: 200, 
+                    body: JSON.stringify(retval)
+                };
+                
+                context.done();
+
                 process.exit();
             }
         );
         
-        request.on('row', function(columns) {
+        request.on('row', function(columns) {            
+            retval += columns;
+            /*
             columns.forEach(function(column) {
                 console.log("%s\t%s", column.metadata.colName, column.value);
             });
+            */
         });
         
-        var result = connection.execSql(request);
-        // console.log(result);
-        return(result);
+        connection.execSql(request);
     }
 };
