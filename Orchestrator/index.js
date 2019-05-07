f425acf8796ec3223fdd6ad61528d50f5ff0ab9a
Vanessa Mastrosimone <vlmastrosimone@gmail.com>
2019-05-07 14:18:30 -0700

    added the route to add a user with email

--- Orchestrator/index.js
+++ Orchestrator/index.js
@@ -5,15 +5,15 @@ var config = require("./creds.js");
 module.exports = function(context, req) {
     var connection = new Connection(config);
     connection.on("connect", function(err) {
-        // routes not needing date 
+        // routes not needing date
         var routes = {
             getAllUsers: "SELECT * FROM Users",
             getAllRobots: "SELECT * FROM Robots",
             getAllTimeSlots: "SELECT * FROM TimeSlots",
             getAllReservations: "SELECT * FROM Reservation",
-            
+
             addUserOld: "INSERT INTO Users (uID) VALUES (5)",
-            addUser: "INSERT INTO Users DEFAULT VALUES",
+            //addUser: "INSERT INTO Users DEFAULT VALUES",
             addTimeSlotOld: "INSERT INTO TimeSlots (TimeID) VALUES (5)",
             addTimeSlot:
                 "INSERT INTO TimeSlots(IsReserved, StartTime, EndTime) VALUES (0,07:30,10:30)",
@@ -29,47 +29,62 @@ module.exports = function(context, req) {
         // if date is specified, add and set necessary routes / SQL
         if (req.query.date) {
             routes["getReservations"] =
-                "SELECT * FROM Reservation WHERE Reservation.ResDate = '" + 
-                req.query.date + "'";
-            routes["getUsedTimeSlots"] = 
-                "SELECT T.TimeID FROM TimeSlots T, Reservation R WHERE R.ResDate = '" + 
-                req.query.date + "' AND R.TimeID = T.TimeID"
+                "SELECT * FROM Reservation WHERE Reservation.ResDate = '" +
+                req.query.date +
+                "'";
+            routes["getUsedTimeSlots"] =
+                "SELECT T.TimeID FROM TimeSlots T, Reservation R WHERE R.ResDate = '" +
+                req.query.date +
+                "' AND R.TimeID = T.TimeID";
             if (req.query.timeID) {
                 routes["addReservation"] =
-                    "INSERT INTO Reservation(RobotID, uID, TimeID, ResDate) VALUES(1, 1, "
-                    + req.query.timeID + ", '" + req.query.date + "')";
+                    "INSERT INTO Reservation(RobotID, uID, TimeID, ResDate) VALUES(1, 1, " +
+                    req.query.timeID +
+                    ", '" +
+                    req.query.date +
+                    "')";
             }
         }
 
-        // try to exec sql based on specified req.query.func parameters 
+        if (req.query.uEmail)
+            routes["addUser"] =
+                "INSERT INTO Users (uEmail) VALUES ('" +
+                req.query.uEmail +
+                "')";
+
+        // try to exec sql based on specified req.query.func parameters
         if (err) {
             context.log(err);
             context.res = {
                 status: 400,
                 body: "error"
             };
-            context.done(); 
-        } 
+            context.done();
+        }
         if (req.query.func && req.query.func in routes) {
             execDbCommand(routes[req.query.func]);
-        } 
-        else {
+        } else {
             context.res = {
                 status: 400,
-                body: "Please pass a name on the query string or in the request body"
+                body:
+                    "Please pass a name on the query string or in the request body"
             };
             context.done();
         }
 
-        // executes sql request passed in as first parameter 
+        // executes sql request passed in as first parameter
         function execDbCommand(sqlStatement) {
             console.log("Reading rows from the Table...");
             // var retval = "";
 
-            var returnContainer = {};           // Object
+            var returnContainer = {}; // Object
 
             // Read all rows from table
-            var request = new Request(sqlStatement, function(err, rowCount, rows) {
+            var request = new Request(sqlStatement, function(
+                err,
+                rowCount,
+                rows
+            ) {
                 console.log(rowCount + " rows returned");
                 var retval = JSON.stringify(returnContainer);
                 context.res = {
@@ -79,19 +94,19 @@ module.exports = function(context, req) {
                 context.done();
             });
 
-            var iterator = 0
+            var iterator = 0;
             // returnContainer[column.metadata.colName] = column.value;
-            request.on('row', function(columns) {
-                iterator += 1
-                returnContainer[iterator] = columns
+            request.on("row", function(columns) {
+                iterator += 1;
+                returnContainer[iterator] = columns;
             });
-            
+
             connection.execSql(request);
         }
     });
 };
 
-// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
-// change output to return *something* so that it is clear when getUsedTimeSlots does not 
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+// change output to return *something* so that it is clear when getUsedTimeSlots does not
 // have anything to return as intended -- not to be confused with a connection error
-// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
